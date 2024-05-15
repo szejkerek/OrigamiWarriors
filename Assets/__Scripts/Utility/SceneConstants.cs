a718bb75a60900e25100a4768e3fd538c11e02c4
Bartek Gordon <bartekk.gordon@gmail.com>
2024-05-15 13:06:00 +0200

    Create choose level scene

--- Assets/__Scripts/Utility/SceneConstants.cs
+++ Assets/__Scripts/Utility/SceneConstants.cs
@@ -1,5 +1,9 @@
 public static class SceneConstants
 {
     public const int MenuScene = 0;
-    public const int GameplayScene = 1;
+    public const int ChooseMapScene = 1;
+    public const int ManagementScene = 2;
+    public const int Level_1 = 3;
+    public const int Level_2 = 4;
+    public const int Level_3 = 5;
 }
