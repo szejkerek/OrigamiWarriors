b245b25ddb199f080204940ebf86b85c91815c56
Bartek Gordon <bartekk.gordon@gmail.com>
2024-05-17 20:39:23 +0200

    initial draft of items architecture

--- Assets/__Scripts/New/ItemSO.cs
+++ Assets/__Scripts/New/ItemSO.cs
@@ -2,25 +2,68 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
-public abstract class ItemSO : ScriptableObject, IUseable
+public class ThrowableItem : IExecutable
 {
-    [field: SerializeField] public string DisplayName { private set; get; }
-    [field: SerializeField] public Sprite Icon { private set; get; }
-    public abstract void Execute();
+    string displayName;
+    Sprite icon;
+    float throwPower;
+    public ThrowableItem(ThrowableItemSO data)
+    {
+        displayName = data.DisplayName;
+        icon = data.Icon;
+        throwPower = data.ThrowPower;
+    }
+
+    public ThrowableItem(string displayName,Sprite icon,float throwPower)
+    {
+        this.displayName = displayName;
+        this.icon = icon;
+        this.throwPower = throwPower;
+    }
+
+    public void Execute()
+    {
+        Debug.Log($"{displayName} is thrown with power {throwPower}!");
+    }
 }
 
-internal interface IUseable
+public interface IExecutable
 {
     void Execute();
 }
 
-[CreateAssetMenu(fileName = "NewItem", menuName = "CharacterCreator/Items/Throwable")]
-public class ThrowableItemSO : ItemSO
+public class ItemSO : ScriptableObject
 {
-    public override void Execute()
-    {
-        throw new System.NotImplementedException();
-    }
+    [field: SerializeField] public string DisplayName { private set; get; }
+    [field: SerializeField] public Sprite Icon { private set; get; }
 }
 
+[CreateAssetMenu(fileName = "NewThrowableItem", menuName = "CharacterCreator/Items/Throwable")]
+public class ThrowableItemSO : ItemSO
+{
+    [field: SerializeField] public float ThrowPower { private set; get; }
+}
+
+[CreateAssetMenu(fileName = "NewMeleeItem", menuName = "CharacterCreator/Items/Melee")]
+public class MeleeItemSO : ItemSO
+{
+    [field: SerializeField] public float Damage { private set; get; }
+}
+
+[CreateAssetMenu(fileName = "NewAoEItem", menuName = "CharacterCreator/Items/AoE")]
+public class AoEItemSO : ItemSO
+{
+    [field: SerializeField] public float Range { private set; get; }
+}
+
+[CreateAssetMenu(fileName = "NewPassivetem", menuName = "CharacterCreator/Items/Passive")]
+public class PassiveItemSO : ItemSO
+{
+    [field: SerializeField] public float Damage { private set; get; }
+}
+
+[CreateAssetMenu(fileName = "NewEffecttem", menuName = "CharacterCreator/Items/Effect")]
+public class EffectItemSO : ItemSO
+{
+    [field: SerializeField] public float Power { private set; get; }
+}
