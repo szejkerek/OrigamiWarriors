b245d8acde88ac3706c63f7d1a7ba06a658b89f7
Bartek Gordon <bartekk.gordon@gmail.com>
2024-05-18 00:32:05 +0200

    Make character picking

--- Assets/__Scripts/TeamManagementInterface.cs
+++ Assets/__Scripts/TeamManagementInterface.cs
@@ -1,18 +1,24 @@
+using GordonEssentials;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TeamMembersPicker : MonoBehaviour
+public class TeamManagementInterface : Singleton<TeamManagementInterface> 
 {
     public Character currentCharacter;
     [SerializeField] List<TeamMember> charactersSlot;
 
     private void Start()
     {
+        FillStartingCharacters();
+    }
+
+    private void FillStartingCharacters()
+    {
         List<Character> savedCharacters = SavableDataManager.Instance.data.characters;
-        for (int i = 0; i < charactersSlot.Count && i < savedCharacters.Count; i++) {
+        for (int i = 0; i < charactersSlot.Count && i < savedCharacters.Count; i++)
+        {
             charactersSlot[i].SetCharacter(savedCharacters[i]);
         }
-
     }
 }
--- Assets/__Scripts/TeamMember.cs
+++ Assets/__Scripts/TeamMember.cs
@@ -1,6 +1,9 @@
+using GordonEssentials.Types;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.TextCore.Text;
 using UnityEngine.UI;
 
 public class TeamMember : MonoBehaviour
@@ -10,10 +13,40 @@ public class TeamMember : MonoBehaviour
     [SerializeField] Image characterIcon;
 
     Character character;
-    
+
+    private void Awake()
+    {
+        returnBtn.gameObject.SetActive(false);
+        characterBtn.onClick.AddListener(CharacterBehavior);
+        returnBtn.onClick.AddListener(ReturnBehavior);
+    }
+
+    private void ReturnBehavior()
+    {
+        TeamManagementInterface.Instance.currentCharacter = null;
+        character = null;
+        characterIcon.sprite = null;
+        returnBtn.gameObject.SetActive(false);
+    }
+
+    private void CharacterBehavior()
+    {
+        if (character != null)
+        {
+            TeamManagementInterface.Instance.currentCharacter = character;
+        }
+        else
+        {
+            SetCharacter(SavableDataManager.Instance.data.characters.SelectRandomElement());
+        }
+    }
+
     public void SetCharacter(Character character)
     {
         this.character = character;
         characterIcon.sprite = character.icon;
+        returnBtn.gameObject.SetActive(true);
     }
+
+
 }
