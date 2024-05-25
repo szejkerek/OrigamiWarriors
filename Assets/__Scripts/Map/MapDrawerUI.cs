5cc8adf3c2eb0a87385be392f90efa4f32aa76c6
mateusz.gorecki <mategor195@student.polsl.pl>
2024-05-25 18:58:35 +0200

    UI

--- Assets/__Scripts/Map/MapDrawerUI.cs
+++ Assets/__Scripts/Map/MapDrawerUI.cs
@@ -1,11 +1,15 @@
 using GordonEssentials;
 using UnityEngine;
 using Unity.Mathematics;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
 public class MapDrawerUI : Singleton<MapDrawerUI>
 {
     public Map Map { get; protected set; }
     public GameObject contentParent;
     public GameObject nodePrefab;
+    public GameObject linePrefab;
     public RectTransform rectTransform;
     private float contentX, contentY;
 
@@ -23,6 +27,9 @@ public class MapDrawerUI : Singleton<MapDrawerUI>
             Debug.LogWarning("Map was null in MapView.ShowMap()");
             return;
         }
+        Dictionary<Point, GameObject> nodeObjects = new Dictionary<Point, GameObject>();
+
+
 
         foreach (MapNode mapNode in m.mapNodes)
         {
@@ -31,19 +38,43 @@ public class MapDrawerUI : Singleton<MapDrawerUI>
             mapNodeObject.transform.localPosition = CalculatePosition(mapNode.position);
             mapNode.position = mapNodeObject.transform.localPosition;
             Debug.Log("Map location: " + mapNode.locationOnMap + "Map position: " + mapNodeObject.transform.localPosition);
-
+            nodeObjects[mapNode.locationOnMap] = mapNodeObject;
 
         }
-
-       // Map = m;
+        DrawConnections(m, nodeObjects);
+        // Map = m;
     }
 
     private Vector2 CalculatePosition(Vector2 postitionNormalized)
     {
-        float newX = math.remap(0f, 1f, -contentX *3/8, +contentX * 3 / 8, postitionNormalized.x);
-        float newY = math.remap(0f, 1f, -contentY *3/8, +contentY *3/8, postitionNormalized.y);
+        float newX = math.remap(0f, 1f, -contentX * 3 / 8, +contentX * 3 / 8, postitionNormalized.x);
+        float newY = math.remap(0f, 1f, -contentY * 3 / 8, +contentY * 3 / 8, postitionNormalized.y);
 
 
         return new Vector2(newX, newY);
     }
+
+    private void DrawConnections(Map map, Dictionary<Point, GameObject> nodeObjects)
+    {
+        foreach (MapNode mapNode in map.mapNodes)
+        {
+            foreach (Point outNodeLocation in mapNode.outNodesLocations)
+            {
+                if (nodeObjects.TryGetValue(mapNode.locationOnMap, out GameObject fromNodeObject) &&
+                    nodeObjects.TryGetValue(outNodeLocation, out GameObject toNodeObject))
+                {
+                    DrawLine(fromNodeObject.transform.localPosition, toNodeObject.transform.localPosition);
+                }
+            }
+        }
+    }
+
+    private void DrawLine(Vector2 start, Vector2 end)
+    {
+        var lineObject = Instantiate(linePrefab, contentParent.transform);
+        var lineRenderer = lineObject.GetComponent<UILine>();
+
+        lineRenderer.SetPositions(start, end);
+    }
+
 }
