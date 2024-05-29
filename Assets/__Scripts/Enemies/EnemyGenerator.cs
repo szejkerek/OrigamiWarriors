21f1387db0d9764f242e599e37b5bfb6b69e33af
Biurito <oto.wojtek@gmail.com>
2024-05-29 18:51:27 +0200

    Working enemy visuals

--- Assets/__Scripts/Enemies/EnemyGenerator.cs
+++ Assets/__Scripts/Enemies/EnemyGenerator.cs
@@ -1,27 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using GordonEssentials;
 
 public class EnemyGenerator : MonoBehaviour
 {
     public Material baseMaterial;
+    private Material newMaterial;
     public MeshRenderer renderer;
 
     public List<Transform> transforms;
-    //public localScaleMax = 1;
-    public float localScaleMinModifier = 1;
+    public Interval<float> scaleModifier;
 
 
     public void Awake()
     {
-        Material newMaterial = Instantiate(baseMaterial);
+        newMaterial = Instantiate(baseMaterial);
         renderer.materials[0] = newMaterial;
 
         foreach(Transform t in transforms)
         {
-            t.localScale
+            t.localScale = new Vector3(t.localScale.x * scaleModifier.GetValueBetween(), t.localScale.y * scaleModifier.GetValueBetween(), t.localScale.z * scaleModifier.GetValueBetween());
         }
     }
 
+
+    public void GetDamage(float amount)
+    {
+        foreach (Transform t in transforms)
+        {
+            t.localScale = new Vector3(t.localScale.x * amount, t.localScale.y * amount, t.localScale.z * amount);
+        }
+    }
+
+
+    public void Update()
+    {
+        //GetDamage(1 - Time.deltaTime * 0.1f);
+        //newMaterial.SetFloat("_Smooth", 0.5f);
+    }
 }
