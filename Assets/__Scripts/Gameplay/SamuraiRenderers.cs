6481b0b8047df7bad3f3c7342e834e28e90f898b
Bartłomiej Gordon <bartekk.gordon@gmail.com>
2024-05-30 17:55:10 +0200

    Update display of characters

--- Assets/__Scripts/Gameplay/SamuraiRenderers.cs
+++ Assets/__Scripts/Gameplay/SamuraiRenderers.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 [System.Serializable]
 public class SamuraiRenderers
@@ -9,7 +10,28 @@ public class SamuraiRenderers
     public SpriteRenderer Head;
     public SpriteRenderer Weapon;
     public SpriteRenderer Chestplate;
-    public SpriteRenderer Pants;
+    public SpriteRenderer Pants;    
+}
 
-    
+[System.Serializable]
+public class SamuraiImages
+{
+    public Image Helmet_B;
+    public Image Helmet_F;
+    public Image Face_F;
+    public Image Head;
+    public Image Weapon;
+    public Image Chestplate;
+    public Image Pants;
+
+    public void SetActive(bool active)
+    {
+       Helmet_B.gameObject.SetActive(active);
+       Helmet_F.gameObject.SetActive(active);
+       Face_F.gameObject.SetActive(active);
+       Head.gameObject.SetActive(active);
+       Weapon.gameObject.SetActive(active);
+       Chestplate.gameObject.SetActive(active);
+       Pants.gameObject.SetActive(active);
+    }
 }
