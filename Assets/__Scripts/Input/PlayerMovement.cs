f081f1fcfb8189b6a0b81ab3682aaf9f097d1e62
Bartłomiej Gordon <bartekk.gordon@gmail.com>
2024-06-04 12:23:04 +0200

    Make Samurai General use Move Script

--- Assets/__Scripts/Input/PlayerMovement.cs
+++ Assets/__Scripts/Input/PlayerMovement.cs
@@ -7,19 +7,14 @@ public class PlayerMovement : MonoBehaviour
   private Vector3 moveVector3D = Vector3.zero;
   private Rigidbody rb = null;
   private Animator animator = null;
-
-  // adjust the velocity from the editor
   [SerializeField] private float moveSpeed = 5;
-  // adjust the gravity for this specific model
   [SerializeField] private float gravityFactor = 250;
-  // jumping factor
-  [SerializeField] private float jumpingFactor = 50;
 
   private void Awake()
   {
     input = new MovementSchemes();
     rb = GetComponent<Rigidbody>();
-    animator = GetComponent<Animator>();
+    //animator = GetComponent<Animator>();
   }
 
   private void OnEnable()
@@ -28,8 +23,6 @@ public class PlayerMovement : MonoBehaviour
 
     input.Player3D.Movement.performed += OnMovementPerformed;
     input.Player3D.Movement.canceled += OnMovementCancelled;
-
-    input.Player3D.Jump.performed += OnJump;
   }
 
   private void OnDisable()
@@ -38,46 +31,30 @@ public class PlayerMovement : MonoBehaviour
 
     input.Player3D.Movement.performed -= OnMovementPerformed;
     input.Player3D.Movement.canceled -= OnMovementCancelled;
-
-    input.Player3D.Jump.performed -= OnJump;
   }
 
   private void FixedUpdate()
   {
-    //Debug.Log(moveVector3D);
     rb.velocity = moveVector3D * moveSpeed;
-
-    rb.AddForce(Vector3.down * gravityFactor, ForceMode.Acceleration); // force of gravity
+    rb.AddForce(Vector3.down * gravityFactor, ForceMode.Acceleration);
   }
 
-  // function to be called when we press appropriate button
   private void OnMovementPerformed(InputAction.CallbackContext keyValue)
   {
     moveVector3D = keyValue.ReadValue<Vector3>();
-    //rb.AddForce(moveVector3D * moveSpeed, ForceMode.Force);
-
-    // Add the animations based on the direction in which we are moving
     if (moveVector3D != Vector3.zero)
     {
-      animator.SetFloat("Z", 1);
-      animator.SetFloat("X", moveVector3D.x);
-      transform.rotation = Quaternion.LookRotation(moveVector3D);
+     // animator.SetFloat("Z", 1);
+      //animator.SetFloat("X", moveVector3D.x);
+      //transform.rotation = Quaternion.LookRotation(moveVector3D);
     } 
     else
     {
-      animator.SetFloat("X", moveVector3D.x);
-      animator.SetFloat("Z", moveVector3D.z);
+      //animator.SetFloat("X", moveVector3D.x);
+      //animator.SetFloat("Z", moveVector3D.z);
     }
       
   }
 
-  private void OnMovementCancelled(InputAction.CallbackContext keyValue)
-  {
-    moveVector3D = Vector3.zero;
-  }
-
-  private void OnJump(InputAction.CallbackContext keyValue)
-  {
-    rb.AddForce(Vector3.up * jumpingFactor, ForceMode.Impulse);
-  }
+    private void OnMovementCancelled(InputAction.CallbackContext keyValue) => moveVector3D = Vector3.zero;
 }
