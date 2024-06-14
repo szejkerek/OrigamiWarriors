44da82ec9a520c8b5e4c32a3626318b380341f00
Biurito <oto.wojtek@gmail.com>
2024-06-14 21:26:18 +0200

    Goap further implementation

---
+++ Assets/__Scripts/Enemies/EnemyController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public float maxHp = 100;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
