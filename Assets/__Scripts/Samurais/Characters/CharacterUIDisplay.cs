6915205a1b666d0a4e18283b903677bc88f77f52
Bartek Gordon <bartekk.gordon@gmail.com>
2024-06-14 23:47:35 +0200

    Character UI Display

---
+++ Assets/__Scripts/Samurais/Characters/CharacterUIDisplay.cs
@@ -0,0 +1,21 @@
+using TMPro;
+using UnityEngine;
+
+public class CharacterUIDisplay : MonoBehaviour
+{
+    [SerializeField] SamuraiImages viusals;
+    [SerializeField] TMP_Text characterName;
+    [SerializeField] GameObject textDisplay;
+    public void Init(Character character)
+    {
+        viusals.SetActive(true);
+        textDisplay.gameObject.SetActive(true);
+        characterName.text = character.DisplayName;
+        character.SamuraiVisuals.Apply(viusals, character);
+    }
+    public void Clear()
+    {
+        viusals.SetActive(false);
+        textDisplay.gameObject.SetActive(false);
+    }
+}
