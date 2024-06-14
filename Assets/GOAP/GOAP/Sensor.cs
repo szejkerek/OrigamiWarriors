d8b136631565ef59c4ec724dc6ca6a1ee733387d
Biurito <oto.wojtek@gmail.com>
2024-06-14 19:54:43 +0200

    Animations + GOAP upgrade

--- Assets/GOAP/GOAP/Sensor.cs
+++ Assets/GOAP/GOAP/Sensor.cs
@@ -14,46 +14,69 @@ public static class GameObjectExtensions
 [RequireComponent(typeof(SphereCollider))] // every sensor has to have a collider of some sort
 public class Sensor : MonoBehaviour
 {
-  [SerializeField] float detectionRadius = 5.0f; // radius of the sensor
-  [SerializeField] float timerInterval = 1.0f; // how often to check the sensor (instead of every frame)
+    [SerializeField] float detectionRadius = 5.0f; // radius of the sensor
+    [SerializeField] float timerInterval = 1.0f; // how often to check the sensor (instead of every frame)
+    [SerializeField] List<string> targetTags;
+    private List<GameObject> targetsInRange;
 
-  SphereCollider detectionRange;
+    SphereCollider detectionRange;
 
-  public event Action OnTargetChanged = delegate { };
+    public event Action OnTargetChanged = delegate { };
 
-  public Vector3 TargetPosition => target ? target.transform.position : Vector3.zero;
-  public bool IsTargetInRange => TargetPosition != Vector3.zero;
+    public Vector3 TargetPosition => target ? target.transform.position : Vector3.zero;
+    public bool IsTargetInRange => TargetPosition != Vector3.zero;
 
-  GameObject target;
-  Vector3 lastKnownPosition;
-  CountdownTimer timer;
+    GameObject target;
+    Vector3 lastKnownPosition;
+    CountdownTimer timer;
 
-  void Awake()
-  {
-    detectionRange = GetComponent<SphereCollider>();
-    detectionRange.isTrigger = true;
-    detectionRange.radius = detectionRadius;
-  }
-
-  void Start()
-  {
-    timer = new CountdownTimer(timerInterval);
-    timer.OnTimerStop += () =>
+    void Awake()
     {
-      UpdateTargetPosition(target.OrNull());
-      timer.Start();
-    };
-    timer.Start();
-  }
+        if (targetTags == null) targetTags = new List<string>();
+        targetsInRange = new List<GameObject>();
+        detectionRange = GetComponent<SphereCollider>();
+        detectionRange.isTrigger = true;
+        detectionRange.radius = detectionRadius;
+    }
 
-  private void Update()
-  {
-    timer.Tick(Time.deltaTime);
-  }
+    void Start()
+    {
+        timer = new CountdownTimer(timerInterval);
+        timer.OnTimerStop += () =>
+        {
+            UpdateTargetPosition();
+            timer.Start();
+        };
+        timer.Start();
+    }
 
-  void UpdateTargetPosition(GameObject target = null)
-  {
-    this.target = target;
+    private void Update()
+    {
+        timer.Tick(Time.deltaTime);
+    }
+
+    void UpdateTargetPosition()
+    {
+        Debug.Log(targetsInRange.Count);
+        if (targetsInRange.Count == 0) target = null;
+        else
+        {
+            GameObject closestTarget = targetsInRange[0];
+            float minDistance = 0;
+
+            foreach (GameObject t in targetsInRange)
+            {
+                float currDistance = Vector3.Distance(transform.position, t.transform.position);
+                if (currDistance < minDistance)
+                {
+                    minDistance = currDistance;
+                    closestTarget = t;
+                }
+            }
+
+            target = closestTarget;
+        }
+
     if (IsTargetInRange && (lastKnownPosition != TargetPosition || lastKnownPosition != Vector3.zero))
     {
       lastKnownPosition = TargetPosition;
@@ -64,16 +87,28 @@ public class Sensor : MonoBehaviour
   //trigger collider for the player (JUST the player)
   void OnTriggerEnter(Collider other)
   {
-    if (!other.CompareTag("Player")) { return; }
-    UpdateTargetPosition(other.gameObject);
+    bool hasPropperTag = false;
+    foreach (string s in targetTags) if (other.CompareTag(s)) hasPropperTag = true;
+
+    if (!hasPropperTag) { return; }
+
+    if (!targetsInRange.Contains(other.gameObject)) targetsInRange.Add(other.gameObject);
+
+    UpdateTargetPosition();
   }
   
   //trigger collider for the player (JUST the player)
   void OnTriggerExit(Collider other)
   {
-    if (!other.CompareTag("Player")) { return; }
-    UpdateTargetPosition();
-  }
+        bool hasPropperTag = false;
+        foreach (string s in targetTags) if (other.CompareTag(s)) hasPropperTag = true;
+
+        if (!hasPropperTag) { return; }
+
+        if (targetsInRange.Contains(other.gameObject)) targetsInRange.Remove(other.gameObject);
+
+        UpdateTargetPosition();
+    }
 
   private void OnDrawGizmos() // visible when target is in range
   {
