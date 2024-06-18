aa1970f48d18732233dcd4c4fcab3154957aa441
mateusz.gorecki <mategor195@student.polsl.pl>
2024-06-18 23:14:00 +0200

    Fix Managment bugs

--- Assets/__Scripts/UI/TeamManagementInterface.cs
+++ Assets/__Scripts/UI/TeamManagementInterface.cs
@@ -9,7 +9,6 @@ public class TeamManagementInterface : Singleton<TeamManagementInterface>
     public CharacterPanel CharacterPanel;
 
     [SerializeField] TMP_Text money;
-    [SerializeField] Button moneyButton;
     [SerializeField] Button returnBtn;
 
     [SerializeField] CharacterView CharacterViewPrefab;
@@ -29,7 +28,6 @@ public class TeamManagementInterface : Singleton<TeamManagementInterface>
         UpdateResourcesDisplay(SavableDataManager.Instance.data.playerResources);
         FillCharacterSlots();
 
-        moneyButton.onClick.AddListener(() => SavableDataManager.Instance.data.playerResources.AddMoney(500));
     }
 
     private void OnDisable()
