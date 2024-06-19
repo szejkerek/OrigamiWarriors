9e2e6d7a186c0461426c6709c789b3caece8be6e
mateusz.gorecki <mategor195@student.polsl.pl>
2024-06-19 20:50:56 +0200

    Tooltip on Choose new ally

--- Assets/__Scripts/UI/Popup/ChoiceUI.cs
+++ Assets/__Scripts/UI/Popup/ChoiceUI.cs
@@ -1,4 +1,5 @@
 using System;
+using Unity.VisualScripting.Antlr3.Runtime.Misc;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
 using UnityEngine.UI;
@@ -9,16 +10,27 @@ public class ChoiceUI : MonoBehaviour
     [SerializeField] Button choiceBtn;
     [SerializeField] GameObject selectedBorder;
     [SerializeField] CharacterUIDisplay characterDisplay;
+    [SerializeField] TooltipTrigger tooltipTrigger;
 
     Character choiceItem;
 
     public void Init(Character character)
     {
+        
         selectedBorder.SetActive(false);
         this.choiceItem = character;
         characterDisplay.Init(character);
         choiceBtn.onClick.AddListener(SelectChoice);
         OnCharacterSelected += DisableBorder;
+
+        CharacterStats stats = character.GetStats();
+        string content = $"Damage: {stats.Damage:0}\n" +
+        $"Health: {stats.MaxHealth}\n" +
+        $"Armor: {stats.Armor:0}\n" +
+               $"Movement Speed: {stats.Speed:0}\n" +
+               $"Crit Chance: {stats.CritChance * 100:0}%\n";
+
+        tooltipTrigger.SetContent(character.Name, content);
     }
 
     public void EnableBorder()
